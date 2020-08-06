faca60b86ad55cb9d0f6e3f25dc1b584f2198e11
s1270001-gorokichi <49747881+s1270001-Aida@users.noreply.github.com>
2020-08-06 20:00:07 +0900

    User name added

--- coinF.c
+++ coinF.c
@@ -1,27 +1,29 @@
-#include <time.h>
-int main()
-{
-  int i,b=0,heads=0,tails=0;
-  char a[10];
-
-  printf("Tossing a coin...\n");
-  srand(time(NULL));
-  for(i=1;i<4;i++)
-    {
-      b=rand()%2+1;
-      if(b==1)
+	#include <time.h>
+	int main()
 	{
-	  printf("round%d:Heads\n",i);
-	  heads++;	  
-	}
-      else if(b==2)
-	{
-	  printf("round%d:Tails\n",i);
-	  tails++;
-	}
-     
-    }
-  printf("Heads:%d, Tails:%d\n",heads,tails);
+	  int i,b=0,heads=0,tails=0;
+	  char a[10];
+	 printf("Who are you?\n");
+	  scanf("%s",a);
+	  printf("Hello,%s!\n",a);
+	  printf("Tossing a coin...\n");
+	  srand(time(NULL));
+	  for(i=1;i<4;i++)
+	    {
+	      b=rand()%2+1;
+	      if(b==1)
+		{
+		  printf("round%d:Heads\n",i);
+		  heads++;	  
+		}
+	      else if(b==2)
+		{
+		  printf("round%d:Tails\n",i);
+		  tails++;
+		}
 
-  return 0;
-}
+	    }
+	  printf("Heads:%d, Tails:%d\n",heads,tails);
+
+	  return 0;
+	}
